6f52dc1cdb304ab05a37b7f9c97710cfe743e799
Willem Dijkstra <wpd@xs4all.nl>
2025-03-01 20:31:03 +0100

    Add clangd, start looking at using dt from c

--- zmk-config/boards/arm/5x5x3/include/dt-bindings/zmk/lightgroup.h
+++
@@ -1,7 +0,0 @@
-#define LG_BATTERY    00
-#define LG_DESKTOP    01
-#define LG_ENDPOINT   02
-#define LG_HID        03
-#define LG_LAYER      04
-#define LG_PROFILE    05
-#define LG_VOLUME     06
--- zmk-config/src/light-group.c
+++ zmk-config/src/light-group.c
@@ -6,12 +6,17 @@
 
 #include <zephyr/logging/log.h>
 
+#include <color.h>
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
 
-BUILD_ASSERT(DT_NODE_EXISTS(DT_PATH(ledlayout)),
-             "ledlayout not found for LIGHT_GROUP");
+BUILD_ASSERT(DT_NODE_EXISTS(DT_PATH(ledlayout)), "ledlayout not found for LIGHT_GROUP");
 
-const u_int8_t ledlayout[DT_PROP_LEN(DT_PATH(ledlayout), bindings)] =
-    DT_PROP(DT_PATH(ledlayout), bindings);
+static const u_int8_t ledlayout_num = DT_PROP_LEN(DT_PATH(ledlayout), bindings);
+static const u_int8_t ledlayout[DT_PROP_LEN(DT_PATH(ledlayout), bindings)] =
+        DT_PROP(DT_PATH(ledlayout), bindings);
+
+static const hsv_t background[DT_PROP_LEN(DT_PATH(colorgroup, backgrounds), colors)] = {
+        DT_FOREACH_PROP_ELEM_SEP(DT_PATH(colorgroup, backgrounds), colors, DT_PROP_BY_IDX, (,)) };
