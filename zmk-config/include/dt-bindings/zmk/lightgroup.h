2869f566a2de045b2ba332dbb0ecbbddc5a5f1e0
Willem Dijkstra <wpd@xs4all.nl>
2026-05-17 12:29:10 +0200

    Add color and lightgroup definitions

---
+++ zmk-config/include/dt-bindings/zmk/lightgroup.h
@@ -0,0 +1,8 @@
+#define LG_BACKGROUND 00
+#define LG_BATTERY    01
+#define LG_DESKTOP    02
+#define LG_ENDPOINT   03
+#define LG_HID        04
+#define LG_LAYER      05
+#define LG_PROFILE    06
+#define LG_VOLUME     07
